652622faab5aa87e6a8763badcb18390af128b6d
zengqingzhang <zengqingzhang@credit91.com>
2019-10-28 20:04:44 +0800

    test

---
+++ app/common/CurlHelp.class.php
@@ -0,0 +1,136 @@
+<?php
+
+class CurlHelp{
+
+	//连接超时
+	private static $connect_timeout = 20;
+	//错误编码
+	public static $err_code = 0;
+	//错误信息,无错误为''
+	public static $err_msg = '';
+	public static $http_info;
+	public static $http_code;
+	public static $http_header;
+	private static $begin_time = 0;
+
+	/**
+	 * 初始化信息
+	 */
+	private static function init()
+	{
+	    self::$err_code = 0;
+	    self::$err_msg = '';
+	    self::$http_code = 200;
+	    self::$http_header = array();
+	    self::$http_info = array();
+	}
+
+	//get调用第三方接口：
+	public static function curlGet($url ,$param ,$timeout = 2 , $json = true)
+	{
+	    if($param){
+	        $querystring = http_build_query($param);
+	        $url = $url.'?'.$querystring;
+	    }
+	    //初始化
+	    $ch = curl_init();
+	    // 请求头，可以传数组
+	    curl_setopt($ch, CURLOPT_URL,$url);
+	    curl_setopt($ch, CURLOPT_HEADER, false);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // 执行后不直接打印出来
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // 不从证书中检查SSL加密算法是否存在
+	    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+	    $output = curl_exec($ch); //执行并获取HTML文档内容
+	    curl_close($ch); //释放curl句柄
+	    return $json ? @json_decode($output , true) : $output;
+	}
+
+	//post调用第三方接口:
+    public static function cURLHTTPPost($url, $post_data, $timeout = 3, $host = '', $header_append = array(), $failOnError = true)
+    {
+        // self::clearError();
+        if (is_array($post_data)) {//$post_data为数组时，即上传文件时
+            $data_len = count($post_data);
+            $header = array();
+        } else {
+            $data_len = strlen($post_data);
+            $header = array('Content-transfer-encoding: text', 'Content-Length: ' . $data_len);
+        }
+
+        if (!empty($header_append)) {
+            foreach ($header_append as $v) {
+                $header[] = $v;
+            }
+        }
+        if (!empty($host)) {
+            $header[] = 'Host: ' . $host;
+        }
+
+        $curl_handle = curl_init();
+
+        // 连接超时
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, $timeout);
+        // 执行超时
+        curl_setopt($curl_handle, CURLOPT_TIMEOUT, 300);
+        // HTTP返回错误时, 函数直接返回错误
+        curl_setopt($curl_handle, CURLOPT_FAILONERROR, $failOnError);
+        // 允许重定向
+        curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, true);
+        // 允许重定向的最大次数
+        curl_setopt($curl_handle, CURLOPT_MAXREDIRS, 2);
+        // ssl验证host
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, FALSE);
+        // 返回为字符串
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+        // 设置HTTP头
+        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $header);
+        // 指定请求地址
+        curl_setopt($curl_handle, CURLOPT_URL, $url);
+        //设置为post方式
+        curl_setopt($curl_handle, CURLOPT_POST, TRUE);
+        //post 参数
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $post_data);
+        // 执行请求
+        $response = curl_exec($curl_handle);
+        if ($response === false) {
+            self::$err_code = 10616;
+            self::$err_msg = 'cURL errno: ' . curl_errno($curl_handle) . '; error: ' . curl_error($curl_handle);
+            // 关闭连接
+            curl_close($curl_handle);
+
+            return false;
+        }
+        // 关闭连接
+        curl_close($curl_handle);
+
+        return $response;
+    }
+
+    public static function curl_get_302($url) {
+        $ch = curl_init();
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL,  $url);
+        curl_setopt($ch,  CURLOPT_FOLLOWLOCATION, 1); // 302 redirect
+        $data = curl_exec($ch);
+        $Headers =  curl_getinfo($ch);
+        curl_close($ch);
+        if ($data != $Headers)
+            return  $Headers["url"];
+        else
+            return false;
+    }
+
+    public static function printNx($arr, $index, $default = ''){
+
+        if (count($index) == 2) {
+            return !empty($arr[$index[0]][$index[1]]) ? $arr[$index[0]][$index[1]] : $default;
+        } else {
+            return !empty($arr[$index[0]]) ? $arr[$index[0]] : $default;
+        }
+    }
+
+}
+
+?>
+
---
+++ app/ctrl/test/Test.class.php
@@ -0,0 +1,91 @@
+<?php
+
+
+class Test
+{
+
+    private static $base_url = 'http://47.98.188.59/game01/mock/test/?act=Test&st=call_back';
+    private static $mid = 938;
+    public static $token = 'cc47e8894d43f260e143d70994267946';
+
+    //test
+    public function call(){
+
+        $params = [
+            'act' => 'fast_reg_utoken',
+            'act_type' => 1,
+            'mid' => 938,
+            'resource_id' => 49,
+            'from' => "h5ios",
+            'version' => '2.0',
+            'sign' => "800ad0b15fac87393a1e445718642a94",
+        ];
+
+        $res = self::curlGet(self::$base_url, $params);
+        // echo "<br>==========00===============<br>";
+        print_r($res);
+        // echo "<br>==========01===============<br>";
+        //		// echo json_encode($res,JSON_UNESCAPED_UNICODE);
+
+        $prize_lists = CurlHelp::printNx($res, ['data', 'prize_lists'], []);
+        if (empty($prize_lists)) throw new UserException('有效数据为空~！');
+
+        foreach ($prize_lists as $v) {
+            $data['prize_id'] = $v['prize_id'];
+            $data['title'] = $v['title'];
+            $data['task_id'] = $v['task_id'];
+            $data['task_type'] = $v['type'];
+            $data['task_prize'] = $v['task_prize'];
+            $data['deal_prize'] = $v['deal_prize'];
+            $data['task_prize_coin'] = $v['task_prize_coin'];
+            echo "=============begin=============";
+            print_r($data);
+            echo "=============end=============";
+        }
+
+    }
+
+    function call_back(){
+
+        header('Content-Type:application/json');
+        $params = [
+            "code"=>1,
+            "msg" => "ok",
+            "data" => [
+                "totize"=>0.6,
+                "total_prize_coin"=>0.6,
+                "prize_lists" => [
+                    [
+                        "name" => "赤壁斗地主iOS3期",
+                        "type"=>1,
+                        "prize_id"=>rand(10000000,99999999),
+                        "prize_time"=>time(),
+                        "task_id"=>88145,
+                        "title" => "手机注册并试玩3局(新用户专享)",
+                        "ad_id"=>13865,
+                        "task_prize"=>0.6,
+                        "task_prize_coin"=>0.6,
+                        "deal_prize" => 0.6
+                    ],
+                    [
+                        "name" => "赤壁斗地主iOS3期",
+                        "type"=>1,
+                        "prize_id"=>rand(10000000,99999999),
+                        "prize_time"=>time(),
+                        "task_id"=>88146,
+                        "title" => "手机注册并试玩10局(新用户专享)",
+                        "ad_id"=>13865,
+                        "task_prize"=>0.8,
+                        "task_prize_coin"=>0.8,
+                        "deal_prize" => 0.8
+                    ]
+                ]
+            ]
+        ];
+
+        $arr_res = json_encode($params,JSON_UNESCAPED_UNICODE);
+        echo $arr_res;
+    }
+
+
+}
